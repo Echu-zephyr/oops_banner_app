64beaf880a4bc9c47f71a309d25d95d3d8722510
Eshaan Kalpurath <ek7217@srmist.edu.in>
2026-02-16 13:50:44 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -7,10 +7,10 @@
 public class OOPSBannerApp {
     public static void main(String[] args) {
         // Use Case 2: Render OOPS as Banner using Print Statements
-        System.out.println("  *** *** **** **** ");
-        System.out.println(" * * * * * * * ");
-        System.out.println(" * * * * **** *** ");
-        System.out.println(" * * * * * *");
-        System.out.println("  *** *** * **** ");
+        System.out.println("  ***    ***   ****  ****   *****  ");
+        System.out.println(" * * *  *   *  *   * *   * *     ");
+        System.out.println(" * * *  *   *  ****  ****   ****    ");
+        System.out.println(" * * *  *   *  *     *           *   ");
+        System.out.println("  ***    ***   *     *      ****       ");
     }
 }
